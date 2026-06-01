12ce69d4fec4fb01c81e4d9962bd48cf8467e1c5
malikzeeshanzeeshan488-cell <malikzeeshanzeeshan488@gmail.com>
2026-06-01 10:10:16 -0700

    Update code main .py

--- code/code main .py
+++ code/code main .py
@@ -1,1 +1,19 @@
 
+import random
+
+class CustomRandomGenerator:
+    def __init__(self):
+        self.generated_numbers = []
+
+    def generate_number(self, start, end):
+        number = random.randint(start, end)
+        self.generated_numbers.append(number)
+        return number
+
+    def show_history(self):
+        return self.generated_numbers
+
+generator = CustomRandomGenerator()
+
+print("Random Number:", generator.generate_number(1, 100))
+print("History:", generator.show_history())
